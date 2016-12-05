0e9f4ed1b98ea869147c209579b4206d9d10c390
unknown <hmphrmsn@gmail.com>
2016-12-05 14:35:54 +0300

    Function to calculate progress

--- learning_map.py
+++ learning_map.py
@@ -1,3 +1,7 @@
-def learnig_progress():
+def learnig_progress(skills_done):
 	#generate the percentage of the progress
-	
+	done = len(skills_done) 
+	all_skills = len(new_list)
+	progress = done / all_skills
+
+	return progress
